d1cdd0930c0413bf32278e170bfbbabeadd5d5d5
kingtuler <morozovsergey.03@gmail.com>
2022-10-16 18:01:19 +0400

    third script without del

--- first_script.py
+++ first_script.py
@@ -7,7 +7,7 @@ def first_script():
     out_directory = os.path.dirname(__file__)
     with open("classmates.csv", mode="w", encoding='utf-8') as w_file:
         file_writer = csv.writer(w_file, delimiter = ",", lineterminator="\r")
-        file_writer.writerow(["Абсолютный путь к файлу", "Относительный путь к файлу", "номер звеззды"])
+        file_writer.writerow(["Абсолютный путь к файлу", "Относительный путь к файлу", "номер звезды"])
         for star in range(6):
             directory=os.path.join(out_directory,"dataset",str(star))
             for dirs,folder,files in os.walk(directory):
--- third_script.py
+++ third_script.py
@@ -1,7 +1,23 @@
 """Написать скрипт, создающий копию датасета таким образом, чтобы каждый файл из сходного датасета получил случайный номер от 
 0 до 10000, и датасет представлял собой следующую структуру dataset/номер.jpg. Для того чтобы осталась возможность определить 
 принадлежность экземпляра к классу создать файл-аннотацию (как в пункте 1)."""
-
-
+import os
+import random
+import csv
 def third_script():
+    names=[i for i in range(10000)]
+    out_directory = os.path.dirname(__file__)
     
+    if os.path.isdir("third_dataset")==False: os.makedirs("third_dataset")
+    with open("third_script_classmates.csv", mode="w", encoding='utf-8') as w_file:
+        file_writer = csv.writer(w_file, delimiter = ",", lineterminator="\r")
+        file_writer.writerow(["Абсолютный путь к файлу", "Относительный путь к файлу", "номер звезды"])
+        for element in os.listdir("second_dataset"):
+            name=random.choice(names)
+            
+            with open(os.path.join("second_dataset",element), "rb") as f:
+                text=f.read()
+            with open(os.path.join("third_dataset",str(name) ),"wb") as f:
+                f.write(text)
+            directory=os.path.join(out_directory,"third_dataset",str(name)+".txt")   
+            file_writer.writerow([directory,os.path.join("second_dataset", element),element[0]])
