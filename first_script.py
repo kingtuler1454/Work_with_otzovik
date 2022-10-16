0aaeaf9eed8ea70cf4252b6ad49006ba30d43de9
kingtuler <morozovsergey.03@gmail.com>
2022-10-16 14:49:35 +0400

    d

---
+++ first_script.py
@@ -0,0 +1,16 @@
+import os
+import csv
+from tqdm.notebook import tqdm_notebook
+
+
+def first_script():
+    out_directory = os.path.dirname(__file__)
+    with open("classmates.csv", mode="w", encoding='utf-8') as w_file:
+        file_writer = csv.writer(w_file, delimiter = ",", lineterminator="\r")
+        file_writer.writerow(["Абсолютный путь к файлу", "Относительный путь к файлу", "номер звеззды"])
+        for star in range(6):
+            directory=os.path.join(out_directory,"dataset",str(star))
+            for dirs,folder,files in os.walk(directory):
+                for element in files:
+                    print('\nПолный путь к файлу: '+str(dirs)+"/"+element)
+                    file_writer.writerow([str(dirs)+"/"+element,"dataset"+'/'+str(star)+"/"+element, star])
